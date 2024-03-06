234b7d66b31e2ee03f58000f6a7978665795bb3e
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2024-03-07 00:21:18 +0100

    Add new files and configurations for Astro project

---
+++ src/components/Count.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button on:click={handleClick}>
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
+</button>
+<p>Here is the count value: {count}</p>
+
+<style>
+  p {
+    display: grid;
+    color: orangered;
+    font-weight: 500;
+    font-size: 23px;
+    font-family: "Courier New", Courier, monospace;
+  }
+</style>
